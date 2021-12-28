9b4be69cee5fe96308b2a8ee1580320ea3e12c36
Laurrx <c.lauralex1@gmail.com>
2021-12-28 22:07:10 +0200

    ayaye

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+function addScore(tableId) {
+    let tableData = document.getElementById("table_" + tableId);
+    let tableInputs = tableData.getElementsByTagName("input");
+    document.getElementById("totalScore_" +tableId).innerHTML = "SCOR: 0;";
+    let tableScore = 0;
+
+    for(htmlEle in tableInputs ){
+
+        if(tableInputs[htmlEle].value != undefined && 
+        tableInputs[htmlEle].value != 0
+        ){
+            tableScore += +tableInputs[htmlEle].value;
+        }
+    }
+    document.getElementById("totalScore_" +tableId).innerHTML = "SCOR: " + tableScore;
+}
+
 function incrementValue(index, maxValue, incrementBy, tableId)
 {
     var value = document.getElementsByClassName('number')[index].value;
@@ -28,7 +45,7 @@ function decrementValue(index, decrementBy, tableId)
 
 function calculateScore() {
     values = document.getElementsByClassName("bi-check-square-fill").length;
-    document.getElementById("iadlScore").innerHTML="SCOR: " + values + "/8";
+    document.getElementById("iadlScore").innerHTML="SCOR: " + values;
 }
 
 function boxChecked(clickedElement) {
@@ -45,4 +62,17 @@ function boxChecked(clickedElement) {
     
     calculateScore();
 
+}
+
+function calculateFinalScore() {
+    let results = document.getElementsByClassName("score");
+    let finalScore = 0;
+    
+
+    for(i=0; i<results.length; i++){
+        finalScore += parseInt(results[i].innerText.substring(6));
+        
+    }
+
+    document.getElementById("finalScoreChange").innerHTML = finalScore;
 }
